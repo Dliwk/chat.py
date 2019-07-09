36eaec2b47eabac4ebdcf584472ea053f3946ceb
RomTrap <roma190505@yandex.ru>
2019-07-09 22:14:24 +0300

    fix

--- chat.py
+++ chat.py
@@ -12,10 +12,12 @@ def send(s):
         snd += i.encode()
     conn.send(snd)
 
-ans = input('1 -- run server, other -- connect')
+name = input("Your name is ")
+ans = input('1 -- run server, other -- connect: ')
 if ans == '1':
+    print("Wait a connection...")
     sock = socket.socket()
-    sock.bind(("0.0.0.0", '17000'))
+    sock.bind(("0.0.0.0", 17000))
     sock.listen(1)
     conn, addr = sock.accept()
     name2 = recv()
@@ -23,4 +25,24 @@ if ans == '1':
     send(name)
     while True:
         send(input(name + ": "))
-        print(name2 + ": " + recv())
+        print(name2 + ": ", end='')
+        a = recv()
+        if a == '':
+            print("Diskonnected")
+            exit(0)
+        print(a)
+else:
+    print(f"Connecting to {ans}...")
+    conn = socket.socket()
+    conn.connect((ans, 17000))
+    send(name)
+    name2 = recv()
+    print(f"Successful connect to {name2} ({ans})")
+    while True:
+        print(name2 + ": ", end='')
+        a = recv()
+        if a == '':
+            print("Diskonnected")
+            exit(0)
+        print(a)
+        send(input(name + ": "))
